bc23a6ecacbee10f4e6c9df25eb27f9203fdc527
RAFAEL GONCALVES DA LUZ <rafael.goncalves05@cs.up.edu.br>
2025-04-08 21:22:17 -0300

    Resolução do exercício 1 sobre Arquivos

---
+++ Arquivos/src/Exercicio01/TesteNomesAleatorios.java
@@ -0,0 +1,35 @@
+package Exercicio01;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class TesteNomesAleatorios {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+ 
+        File dir = new File("..\\POO-Java\\Arquivos");
+        File arq = new File(dir, "sistema-alunos.csv");
+
+        var resultDir = ArquivoUtils.criarDiretorio(dir);
+        System.out.println("Diretorio foi criado? " + resultDir);
+
+        var resultArq =  ArquivoUtils.criarArquivo(arq);
+        System.out.println("Arquivo foi criado? " + resultArq);
+
+        System.out.println("Necessário definir o número de alunos a serem criado!");
+        System.out.print("Informe a quantidade de alunos: ");
+        var qtd =  scanner.nextInt();
+
+        for (int i = 0; i < qtd; i++) {
+            var idade = GerarDadosAleatorios.gerarIdade();
+            var nome = GerarDadosAleatorios.gerarNomeCompleto();
+
+            var pessoa = new Pessoa(nome, idade);
+
+            ArquivoUtils.escrever(arq, pessoa);
+        }
+
+        scanner.close();
+    }
+}
